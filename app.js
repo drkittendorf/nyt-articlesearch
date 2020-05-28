463d3412c5d729396ab2fae7fb4d884c6afa6136
Erik De Luna <codingerik1.gmail.com>
2020-05-27 20:52:49 -0500

    adding parameter for the search term

--- app.js
+++ app.js
@@ -1,15 +1,22 @@
- // we need a jquery cdn
- // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
- let apiKey = 'AO38D6BatnMPCjZz5cOSOjnHOGhlCUm5';
+// we need a jquery cdn
+// <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 
- $.ajax({
-   url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=election&api-key=${apiKey}`,
-   method: 'GET'
- }).then(function(response){
+let apiKey = 'AO38D6BatnMPCjZz5cOSOjnHOGhlCUm5';
 
 
-   console.log(response);
+// article to search this is  where we will put the string so we can search 
+let searchItem = '';
+
+$.ajax({
+    url: `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchItem}&api-key=${apiKey}`,
+    method: 'GET'
+}).then(function (response) {
+
+
+    console.log(response.response.docs);
 
 
 
- });
+});
+
+
