70b477f53255ecab496aa144be9a378876057e25
Erik De Luna <codingerik1.gmail.com>
2020-05-27 20:49:01 -0500

    adding jquery cdn in comments line 2

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
  // we need a jquery cdn
+ // <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
  let apiKey = 'AO38D6BatnMPCjZz5cOSOjnHOGhlCUm5';
 
  $.ajax({
